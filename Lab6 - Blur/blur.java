a9f8f2402bb404835f6499e939d9a715b9566f49
Kryxem <jajli@ucsc.edu>
2015-03-16 20:33:38 -0400

    *blur.java -numPasses argument implemented: Number of times to blur the same image, each image. -Cyclic Barrier implemented: In order for numPasses to work, all threads must finish a image before starting to re-blur, therefore Cyclic Barrier will stop each thread after finishing and the main function will stop until all threads are done. -Moved getting blurBox to its own function -Moved updatePercent to its own function

--- Lab6 - Blur/blur.java
+++ Lab6 - Blur/blur.java
@@ -4,6 +4,8 @@ import java.io.*;
 import javax.imageio.*;
 import java.awt.image.BufferedImage;
 import java.awt.Color;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.locks.*;
 
 class blur{
@@ -14,8 +16,9 @@ class blur{
 	static int blurLength = 25;
 	static BufferedImage img;
 	static BufferedImage tmpImg;
-	static Lock L = new ReentrantLock();
 	static int currentTotalPixels = 0;
+	static CyclicBarrier CB;
+	static Lock L = new ReentrantLock();
 
 	public static void main(String[] args){
 		String pattern = "";
@@ -29,10 +32,13 @@ class blur{
 		}else{
 			System.out.println("Enter filename pattern: ");
 			pattern = scan.nextLine();
+			//pattern = "test%d.png";
 			System.out.println("Enter number of threads: ");
 			threads = scan.nextInt();
+			//threads = 4;
 			System.out.println("Enter number of passes: ");
 			numPasses = scan.nextInt();
+			//numPasses = 2;
 		}
 		long startTime = System.currentTimeMillis();
 		
@@ -44,158 +50,164 @@ class blur{
 		
 		allFiles = getFiles(pattern);
 		
+		threadList = new Thread[threads];
+		
 		if(allFiles.length == 0){
 			System.out.println("No files with \"" + pattern + "\" pattern found!");
-		}/*else if(allFiles.length > 0){
-			for(int x = 0; x < allFiles.length; x++){
-				System.out.println("File: " + allFiles[x].getName());
-			}
-		}
-		*/
-		
-		//Currently will start and run threads for each file one at a time
-		for(File file : allFiles){
-			try{
-				img = ImageIO.read(file);
-				tmpImg = new BufferedImage(img.getWidth(),img.getHeight(),img.getType());
-				currentTotalPixels = img.getWidth() * img.getHeight();
-			} catch(IOException e){
-				System.out.println(e);
-			}
-			
-			System.out.println(file.getName() + " Start!");
-			
-			//------------THREADS-------------
-			threadList = new Thread[threads];
-			
-			//Create threads and start
-			for(int i = 0; i < threads; i++){
-				threadList[i] = new iThread(i, threads, numPasses, blurSize);
-				threadList[i].start();
-			}
-			
-			//Join threads
-			for(int i = 0; i < threads; i++){
-				try{
-					threadList[i].join();
-				} catch(InterruptedException e){
-					return;
+		} else{
+			//Currently will start and run threads for each file one at a time
+			for(File file : allFiles){
+				for(int currPass = 0; currPass < numPasses; currPass++){
+					try{
+						if(currPass == 0){
+							img = ImageIO.read(file);
+							tmpImg = new BufferedImage(img.getWidth(),img.getHeight(),img.getType());
+							currentTotalPixels = img.getWidth() * img.getHeight();
+						} else if(currPass%2 == 1){
+							img = tmpImg;
+						} else{
+							tmpImg = img;
+						}
+					} catch(IOException e){
+						System.out.println(e);
+					}
+					
+					System.out.println(file.getName() + ", Pass: "+ (currPass+1) +" Start!");
+					
+					//------------THREADS-------------
+	
+					//Start threads
+					for(int i = 0; i < threads; i++){
+						threadList[i] = new iThread(i, threads, blurSize);
+						threadList[i].start();
+					}
+					
+					//Join threads
+					for(int i = 0; i < threads; i++){
+						try{
+							threadList[i].join();
+						} catch(InterruptedException e){
+							return;
+						}
+					}
+					
+					CyclicBarrier CB = new CyclicBarrier(threads);
+					
 				}
+				//Test
+				try{
+					File outputfile = new File("BLUR-"+file.getName());
+					ImageIO.write(tmpImg, "JPG", outputfile);
+				}catch(IOException e){
+					System.out.println(e);
+				}
+				
+				System.out.println(file.getName() + " Done!");
 			}
 			
-			//Test
-			try{
-				File outputfile = new File("BLUR-"+file.getName());
-				ImageIO.write(tmpImg, "JPG", outputfile);
-			}catch(IOException e){
-				System.out.println(e);
-			}
-			System.out.println(file.getName() + " Done!");
-			//break;
+			long endTime = System.currentTimeMillis();
+			long time = endTime - startTime;
+			System.out.println("Time: " + time + "ms");
 		}
-		
-		long endTime = System.currentTimeMillis();
-		long time = endTime - startTime;
-		System.out.println("Time: " + time + "ms");
 	}
 	
 	public static class iThread extends Thread{
-		int index, passes, blurSize, start, end, imgWidth;
+		int index, blurSize, start, end, imgWidth;
+		int rStart, rEnd, cStart, cEnd;
 		static int percent = 0;
 		static int oldP = 0;
 		static int count = 0;
 		
-		public iThread(int index, int threads, int passes, int blurSize){
+		public iThread(int index, int threads, int blurSize){
 			this.index = index;
-			this.passes = passes;
 			this.blurSize = blurSize;
-			//try{
-			//	L.lock();
 			this.imgWidth = img.getWidth();
 			this.start = index * img.getHeight()/threads;
 			this.end   = (index+1) * img.getHeight()/threads;
-			//} finally{
-			//	L.unlock();
-			//}
+			CB = new CyclicBarrier(threads);
 		}
 		
 		public void run(){
-			//System.out.println("Thread["+this.index+"] Start: "+this.start);
-			//System.out.println("Thread["+this.index+"] End  : "+this.end);
-			
-			//Goal: For each pixel, get the average RGB value in a 25x25 px box
-			//		and set it to that pixel.  Goes from left to right and then
-			//		down between start and end pixels.
-			int halfBlur = (this.blurSize-1)/2;
-			int rStart,rEnd,cStart,cEnd;
-			//For loops for one pixel
-			//System.out.println("Start!");
-			for(int y = this.start; y < this.end; y++){
-				for(int x = 0; x < this.imgWidth; x++){
-					//Calc start and end rows and columns
-					//System.out.println("Thread["+this.index+"] X["+x+"] Y["+y+"]");
-					if(y - halfBlur >= 0)
-						rStart = y-halfBlur;
-					else
-						rStart = 0;
-					if(y + halfBlur <= this.end)
-						rEnd = y+halfBlur;
-					else
-						rEnd = this.end;
-					if(x - halfBlur >= 0)
-						cStart = x-halfBlur;
-					else
-						cStart = 0;
-					if(x + halfBlur <= this.imgWidth)
-						cEnd = x+halfBlur;
-					else
-						cEnd = this.imgWidth;
-					int pixels = (rEnd-rStart)*(cEnd-cStart);
-					int tmpR = 0;
-					int tmpG = 0;
-					int tmpB = 0;
-					int RGB = 0;
-					//For loops to get 25x25 px area
-					for(int r = rStart; r < rEnd; r++){
-						for(int c = cStart; c < cEnd; c++){
-							//Grab RGB values
-							//try{
-							//	L.lock();
-							RGB = img.getRGB(c,r);
-							//} finally{
-							//	L.unlock();
-							//}
-							tmpR +=((0x00ff0000 & RGB) >> 16);
-							tmpG +=((0x0000ff00 & RGB) >> 8);
-							tmpB +=((0x000000ff & RGB));
-						}
-					}
-					tmpR = tmpR/pixels;
-					tmpG = tmpG/pixels;
-					tmpB = tmpB/pixels;
-					//Set RGB value to new image
-					try{
-						L.lock();
-						tmpImg.setRGB(x, y, new Color(tmpR,tmpG,tmpB).getRGB());
-						count++;
-						percent = count*100/currentTotalPixels;
+			try{
+				int halfBlur = (this.blurSize-1)/2;
+				for(int y = this.start; y < this.end; y++){
+					for(int x = 0; x < this.imgWidth; x++){
+						//If box is smaller, cuts off edges
+						if(y - halfBlur >= 0)
+							rStart = y-halfBlur;
+						else
+							rStart = 0;
+						if(y + halfBlur <= this.end)
+							rEnd = y+halfBlur;
+						else
+							rEnd = this.end;
+						if(x - halfBlur >= 0)
+							cStart = x-halfBlur;
+						else
+							cStart = 0;
+						if(x + halfBlur <= this.imgWidth)
+							cEnd = x+halfBlur;
+						else
+							cEnd = this.imgWidth;
 						
-						if(percent > oldP){
-							System.out.format("%7d"+"/"+"%7d pixels left... %3d%% done\n",count,currentTotalPixels,percent);
-							oldP = percent;
-						}
-						if(percent == 100){
-							count = 0;
-							oldP = 0;
-						}
-						
-					} finally{
-						L.unlock();
+						int blurRGB = getBlurRGB(rStart, rEnd, cStart, cEnd);
+						try{
+							L.lock();
+							//Set RGB value to new image
+							tmpImg.setRGB(x, y, blurRGB);
+							//Updates image completion
+							updatePercent();
+						} finally{
+							L.unlock();
+						}	
 					}
 				}
+				//System.out.println("Thread["+this.index+"] waiting...");
+				CB.await();
+				//System.out.println("Thread["+this.index+"] go!");
+			} catch(InterruptedException e){
+				System.out.println(e);
+			} catch(BrokenBarrierException e){
+				System.out.println(e);
 			}
 		}
 		
+		public int getBlurRGB(int rStart, int rEnd, int cStart, int cEnd){
+			
+			int pixels = (rEnd-rStart)*(cEnd-cStart);
+			int tmpR = 0;
+			int tmpG = 0;
+			int tmpB = 0;
+			int RGB = 0;
+			//For loops to get 25x25 px area
+			for(int r = rStart; r < rEnd; r++){
+				for(int c = cStart; c < cEnd; c++){
+					//Grab RGB values
+					RGB = img.getRGB(c,r);
+					tmpR +=((0x00ff0000 & RGB) >> 16);
+					tmpG +=((0x0000ff00 & RGB) >> 8);
+					tmpB +=((0x000000ff & RGB));
+				}
+			}
+			tmpR = tmpR/pixels;
+			tmpG = tmpG/pixels;
+			tmpB = tmpB/pixels;
+			
+			return new Color(tmpR,tmpG,tmpB).getRGB();
+		}
+		
+		public void updatePercent(){	
+			count++;
+			percent = count*100/currentTotalPixels;
+			if(percent > oldP){
+				System.out.format("%7d"+"/"+"%7d pixels left... %3d%% done\n",count,currentTotalPixels,percent);
+				oldP = percent;
+			}
+			if(percent == 100){
+				count = 0;
+				oldP = 0;
+			}
+		}
 	}
 	
 	public static File[] getFiles(String pattern){
@@ -211,7 +223,6 @@ class blur{
 				temp[index] = new File(filename);
 				index++;
 			}
-			
 		}
 		File[] images = new File[index];
 		for(int i = 0; i < images.length; i++)
